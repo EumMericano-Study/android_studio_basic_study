56e5d4bd428549ebb3df8081d3e6edc003604960
Eum Young Hyun <opwer032@naver.com>
2021-09-11 18:48:16 +0900

    feat: Set login layout

--- example_kotlin_01/app/src/main/java/com/example/example_kotlin_01/MainActivity.kt
+++ example_kotlin_01/app/src/main/java/com/example/example_kotlin_01/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.example_kotlin_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    fun Login(v: View) {
+
+    }
+
 }
