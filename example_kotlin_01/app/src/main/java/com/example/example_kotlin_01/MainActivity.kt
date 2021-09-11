d8e54b5f3ae7c695fc8a761843b07cd7f7951d61
Eum Young Hyun <opwer032@naver.com>
2021-09-11 19:19:25 +0900

    feat: Add login logic

--- example_kotlin_01/app/src/main/java/com/example/example_kotlin_01/MainActivity.kt
+++ example_kotlin_01/app/src/main/java/com/example/example_kotlin_01/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.example_kotlin_01
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -11,10 +15,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        et_age.setOnEditorActionListener{ v, action, event ->
+            if (action == EditorInfo.IME_ACTION_DONE) {
+                login(v)
+                true
+            }
+            false
+        }
     }
 
-    fun Login(v: View) {
+    fun login(v: View) {
 
+        var imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
+
+        if(et_email.text.toString() == "opwer032@naver.com" && et_password.text.toString() == "password"
+            && et_name.text.toString() == "Young" && et_age.text.toString() == "31")
+            {
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+            } else Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
     }
 
 }
