9fc36b6560c3ef0ce5860d855f78debe28be5704
Eum Young Hyun <opwer032@naver.com>
2021-09-12 19:43:10 +0900

    feat: Initial Setting of view binding

--- example_kotlin_02/app/src/main/java/com/example/example_kotlin_02/MainActivity.kt
+++ example_kotlin_02/app/src/main/java/com/example/example_kotlin_02/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.example_kotlin_02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import com.example.example_kotlin_02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding= ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.et.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                println("텍스트 변경 전 " + p0)
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                println("텍스트 변경 중 " + p0)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                println("텍스트 변경 후 " + p0)
+            }
+        })
+
+
+
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
     }
 }
