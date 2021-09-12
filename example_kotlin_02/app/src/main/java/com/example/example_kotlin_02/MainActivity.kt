d0fbb610e9eb0df8c8d5683e55f165581970cc38
Eum Young Hyun <opwer032@naver.com>
2021-09-12 21:00:51 +0900

    feat: Add image change

--- example_kotlin_02/app/src/main/java/com/example/example_kotlin_02/MainActivity.kt
+++ example_kotlin_02/app/src/main/java/com/example/example_kotlin_02/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.example_kotlin_02
 
+import android.content.Intent
+import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.ImageView
 import com.example.example_kotlin_02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -32,8 +37,22 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        binding.ivBasic.setScaleType(ImageView.ScaleType.MATRIX)
 
+        val matrix = Matrix()
+        matrix.postRotate(10f)
+        binding.ivBasic.setImageMatrix(matrix)
 
+    }
+
+    fun LoadImage(v: View) {
+        startActivityForResult(Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) binding.ivBasic.setImageURI(data?.data)
     }
 
     override fun onDestroy() {
