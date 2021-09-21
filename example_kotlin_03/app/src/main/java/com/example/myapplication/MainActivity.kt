2db5b4f38d5581fb5178377c9fe2a872ab90df09
Eum Young Hyun <opwer032@naver.com>
2021-09-21 14:09:08 +0900

    feat: Add binding

--- example_kotlin_03/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ example_kotlin_03/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val Mainbinding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun Click(v: View) {
+
+    }
+
 }
