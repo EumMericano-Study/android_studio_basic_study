d1c8247e9f0558a77b61a73725af29e000e033c6
Eum Young Hyun <opwer032@naver.com>
2021-09-21 16:15:39 +0900

    feat: Test change button color

--- example_kotlin_03/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ example_kotlin_03/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,10 +25,24 @@ class MainActivity : AppCompatActivity() {
         binding.iv2.visibility = View.INVISIBLE
         binding.iv3.visibility = View.INVISIBLE
 
+        binding.btn1.setBackgroundColor(resources.getColor(R.color.design_default_color_primary))
+        binding.btn2.setBackgroundColor(resources.getColor(R.color.design_default_color_primary))
+        binding.btn3.setBackgroundColor(resources.getColor(R.color.design_default_color_primary))
+
         when(v) {
-            binding.btn1 -> binding.iv1.visibility = View.VISIBLE
-            binding.btn2 -> binding.iv2.visibility = View.VISIBLE
-            binding.btn3 -> binding.iv3.visibility = View.VISIBLE
+            binding.btn1 -> {
+                binding.iv1.visibility = View.VISIBLE
+                binding.btn1.setBackgroundColor(resources.getColor(R.color.design_default_color_primary_dark))
+            }
+
+            binding.btn2 -> {
+                binding.iv2.visibility = View.VISIBLE
+                binding.btn2.setBackgroundColor(resources.getColor(R.color.design_default_color_primary_dark))
+            }
+            binding.btn3 -> {
+                binding.iv3.visibility = View.VISIBLE
+                binding.btn3.setBackgroundColor(resources.getColor(R.color.design_default_color_primary_dark))
+            }
         }
     }
 
