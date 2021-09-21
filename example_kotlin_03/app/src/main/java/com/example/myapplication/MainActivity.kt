cacd9a5e8ae33b5dda201451894b73d4a247bd6d
Eum Young Hyun <opwer032@naver.com>
2021-09-21 16:08:25 +0900

    feat: Learn about click event

--- example_kotlin_03/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ example_kotlin_03/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,15 +8,28 @@ import com.example.myapplication.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private var mBinding: ActivityMainBinding? = null
-    private val Mainbinding get() = mBinding!!
+    private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        Click(binding.btn1)
+
+        setContentView(binding.root)
     }
 
     fun Click(v: View) {
+        binding.iv1.visibility = View.INVISIBLE
+        binding.iv2.visibility = View.INVISIBLE
+        binding.iv3.visibility = View.INVISIBLE
 
+        when(v) {
+            binding.btn1 -> binding.iv1.visibility = View.VISIBLE
+            binding.btn2 -> binding.iv2.visibility = View.VISIBLE
+            binding.btn3 -> binding.iv3.visibility = View.VISIBLE
+        }
     }
 
 }
